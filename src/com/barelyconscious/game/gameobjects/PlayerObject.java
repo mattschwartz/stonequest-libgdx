0c05558b670a8d5a1f27ef792f54c31961b792ed
Matt Schwartz <mattschwartz@utexas.edu>
2014-05-09 21:11:37 -0500

    Removed double comments

--- src/com/barelyconscious/game/gameobjects/PlayerObject.java
+++ src/com/barelyconscious/game/gameobjects/PlayerObject.java
@@ -9,17 +9,6 @@
    *                    credit from code that was not written fully by yourself.  
    *                    Please email stonequest.bcgames@gmail.com for issues or concerns.
    * File Description:  
-   ************************************************************************** *//* *****************************************************************************
-   * Project:           StoneQuest
-   * File Name:         PlayerObject.java
-   * Author:            Matt Schwartz
-   * Date Created:      05.09.2014 
-   * Redistribution:    You are free to use, reuse, and edit any of the text in
-   *                    this file.  You are not allowed to take credit for code
-   *                    that was not written fully by yourself, or to remove 
-   *                    credit from code that was not written fully by yourself.  
-   *                    Please email stonequest.bcgames@gmail.com for issues or concerns.
-   * File Description:  
    ************************************************************************** */
 
 package com.barelyconscious.game.gameobjects;
