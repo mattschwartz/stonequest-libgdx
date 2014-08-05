42e9094f0550e95df45e6c7bb73af61d97c8c987
Matt Schwartz <schwamat@gmail.com>
2014-08-04 23:04:49 -0500

    Begin work on SQ Editor in C#

---
+++ SQEditor/GUI/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace SQEditor.GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.importToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.objectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsiNewItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsiNewNPC = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsiNewDoodad = new System.Windows.Forms.ToolStripMenuItem();
+            this.campaignToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.campaignToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.questToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pointOfInterestToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackgroundImage = global::SQEditor.Properties.Resources.menu_border;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(660, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // importToolStripMenuItem
+            // 
+            this.importToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.itemToolStripMenuItem1});
+            this.importToolStripMenuItem.Name = "importToolStripMenuItem";
+            this.importToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.importToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.objectToolStripMenuItem,
+            this.campaignToolStripMenuItem});
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // objectToolStripMenuItem
+            // 
+            this.objectToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsiNewItem,
+            this.tsiNewNPC,
+            this.tsiNewDoodad});
+            this.objectToolStripMenuItem.Name = "objectToolStripMenuItem";
+            this.objectToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.objectToolStripMenuItem.Text = "Object";
+            // 
+            // tsiNewItem
+            // 
+            this.tsiNewItem.Name = "tsiNewItem";
+            this.tsiNewItem.Size = new System.Drawing.Size(152, 22);
+            this.tsiNewItem.Text = "Item";
+            this.tsiNewItem.Click += new System.EventHandler(this.tsiNewItem_Click);
+            // 
+            // tsiNewNPC
+            // 
+            this.tsiNewNPC.Name = "tsiNewNPC";
+            this.tsiNewNPC.Size = new System.Drawing.Size(152, 22);
+            this.tsiNewNPC.Text = "NPC";
+            // 
+            // tsiNewDoodad
+            // 
+            this.tsiNewDoodad.Name = "tsiNewDoodad";
+            this.tsiNewDoodad.Size = new System.Drawing.Size(152, 22);
+            this.tsiNewDoodad.Text = "Doodad";
+            // 
+            // campaignToolStripMenuItem
+            // 
+            this.campaignToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.campaignToolStripMenuItem1,
+            this.zoneToolStripMenuItem,
+            this.questToolStripMenuItem,
+            this.pointOfInterestToolStripMenuItem});
+            this.campaignToolStripMenuItem.Name = "campaignToolStripMenuItem";
+            this.campaignToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.campaignToolStripMenuItem.Text = "Campaign";
+            // 
+            // campaignToolStripMenuItem1
+            // 
+            this.campaignToolStripMenuItem1.Name = "campaignToolStripMenuItem1";
+            this.campaignToolStripMenuItem1.Size = new System.Drawing.Size(158, 22);
+            this.campaignToolStripMenuItem1.Text = "Campaign";
+            // 
+            // zoneToolStripMenuItem
+            // 
+            this.zoneToolStripMenuItem.Name = "zoneToolStripMenuItem";
+            this.zoneToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.zoneToolStripMenuItem.Text = "Zone";
+            // 
+            // questToolStripMenuItem
+            // 
+            this.questToolStripMenuItem.Name = "questToolStripMenuItem";
+            this.questToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.questToolStripMenuItem.Text = "Quest";
+            // 
+            // pointOfInterestToolStripMenuItem
+            // 
+            this.pointOfInterestToolStripMenuItem.Name = "pointOfInterestToolStripMenuItem";
+            this.pointOfInterestToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.pointOfInterestToolStripMenuItem.Text = "Point of Interest";
+            // 
+            // itemToolStripMenuItem1
+            // 
+            this.itemToolStripMenuItem1.Name = "itemToolStripMenuItem1";
+            this.itemToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.itemToolStripMenuItem1.Text = "Import";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(184, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(464, 434);
+            this.panel1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 473);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Editor | StoneQuest";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem importToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem objectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsiNewItem;
+        private System.Windows.Forms.ToolStripMenuItem tsiNewNPC;
+        private System.Windows.Forms.ToolStripMenuItem tsiNewDoodad;
+        private System.Windows.Forms.ToolStripMenuItem campaignToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem campaignToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem zoneToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem questToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pointOfInterestToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem itemToolStripMenuItem1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ SQEditor/GUI/ucObjectList.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace SQEditor.GUI
+{
+    partial class ucObjectList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Items");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("NPCs");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Doodads");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Objects", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3});
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Campaign");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Zones");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Quests");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Point of Interest");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Campaign", new System.Windows.Forms.TreeNode[] {
+            treeNode5,
+            treeNode6,
+            treeNode7,
+            treeNode8});
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Node3";
+            treeNode1.Text = "Items";
+            treeNode2.Name = "Node5";
+            treeNode2.Text = "NPCs";
+            treeNode3.Name = "Node6";
+            treeNode3.Text = "Doodads";
+            treeNode4.Name = "Node2";
+            treeNode4.Text = "Objects";
+            treeNode5.Name = "Node9";
+            treeNode5.Text = "Campaign";
+            treeNode6.Name = "Node11";
+            treeNode6.Text = "Zones";
+            treeNode7.Name = "Node12";
+            treeNode7.Text = "Quests";
+            treeNode8.Name = "Node13";
+            treeNode8.Text = "Point of Interest";
+            treeNode9.Name = "Node7";
+            treeNode9.Text = "Campaign";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode4,
+            treeNode9});
+            this.treeView1.Size = new System.Drawing.Size(285, 159);
+            this.treeView1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(222, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(12, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Edit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Location = new System.Drawing.Point(93, 177);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // ucObjectList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 212);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(325, 250);
+            this.Name = "ucObjectList";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Objects";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
